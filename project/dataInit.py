be040a237fbc68acdd1f408676ed152137c1fde6
MosheBiran <73993888+MosheBiran@users.noreply.github.com>
2021-04-20 23:57:09 +0300

    merge info+info

--- project/dataInit.py
+++ project/dataInit.py
@@ -49,6 +49,7 @@ def select_task_by_table(conn, table):
     for row in rows:
         print(row)
 
+
 def create_table(cursor):
     cursor.execute('''create table data (field1 real, field2 real, field3 real,    field4 real)''')
     cursor.commit()
@@ -68,21 +69,31 @@ def init():
     # create a database connection
     conn = create_connection(database)
     cursor = conn.cursor()
+    # create DF
+    data_matchDF = pd.read_sql_query(
+        'SELECT home_team_api_id,away_team_api_id,season,stage,date,home_team_goal,away_team_goal from Match', conn)
 
-    data_matchDF = pd.read_sql_query('SELECT home_team_api_id,away_team_api_id,season,stage,date,home_team_goal,away_team_goal from Match',conn)
-    data_Team_AttrDF = pd.read_sql_query('SELECT team_api_id,date,buildUpPlaySpeedClass,buildUpPlayDribblingClass,buildUpPlayPassingClass,buildUpPlayPositioningClass,defencePressureClass,defenceAggressionClass from Team_Attributes',conn)
+    data_Team_AttrDF = pd.read_sql_query(
+        'SELECT team_api_id,date,buildUpPlaySpeedClass,buildUpPlayDribblingClass,buildUpPlayPassingClass,'
+        'buildUpPlayPositioningClass,defencePressureClass,defenceAggressionClass from Team_Attributes',
+        conn)
+
+    # clean date
     data_matchDF['date'] = data_matchDF['date'].str.slice(stop=4)
     data_Team_AttrDF['date'] = data_Team_AttrDF['date'].str.slice(stop=4)
 
-    data_matchDF = data_matchDF.sort_values(by=['home_team_api_id','date'])
-    data_Team_AttrDF = data_Team_AttrDF.sort_values(by=['team_api_id','date'])
+    # sorting by relevant col
+    data_matchDF = data_matchDF.sort_values(by=['home_team_api_id', 'date'])
+    data_Team_AttrDF = data_Team_AttrDF.sort_values(by=['team_api_id', 'date'])
 
-    new_df = pd.merge(data_matchDF, data_Team_AttrDF, how='inner', left_on=[ 'date','home_team_api_id'], right_on=[ 'date','team_api_id'])
-    new_df = pd.merge(new_df, data_Team_AttrDF, how='inner', left_on=[ 'date','away_team_api_id'], right_on=[ 'date','team_api_id'])
-    # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    # merging first by ['date', 'home_team_api_id'] and again by ['date', 'away_team_api_id']
+    new_df = pd.merge(data_matchDF, data_Team_AttrDF, how='inner', left_on=['date', 'home_team_api_id'],
+                      right_on=['date', 'team_api_id'])
+    new_df = pd.merge(new_df, data_Team_AttrDF, how='inner', left_on=['date', 'away_team_api_id'],
+                      right_on=['date', 'team_api_id'])
+
+    # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     #     print(new_df)
 
     cursor.close()
     conn.close()
-
-
