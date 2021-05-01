2ca05b2e506ece0d87144cf902ed80dc6d74d56f
MosheBiran <73993888+MosheBiran@users.noreply.github.com>
2021-05-01 16:10:18 +0300

    # Adding team long name

--- project/dataInit.py
+++ project/dataInit.py
@@ -6,6 +6,8 @@ from scipy.interpolate import rbf
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 
+path = "C:\\Users\\biran\\Desktop\\3\\database.sqlite\\"
+
 
 def create_connection(db_file):
     """ create a database connection to the SQLite database
@@ -58,7 +60,7 @@ def create_table(cursor):
     cursor.commit()
 
 
-def save2CVS(database_after_clean, file_path):
+def save2CSV(database_after_clean, file_path):
     database_after_clean.to_csv(file_path + "database_after_clean.csv")
 
 
@@ -102,8 +104,14 @@ def dataframe_filter_null(data_match_df, data_team_attr_df):
     return new_df_outer
 
 
+def test_train_models_split(new_df):
+    df_2012_2013_2014 = new_df.loc[(new_df['season'].isin(["2012/2013", "2013/2014", "2014/2015"]))]
+    df_2015_2016 = new_df.loc[(new_df['season'].isin(["2015/2016"]))]
+    df_2012_2013_2014.to_csv(path + "df_2012_2013_2014.csv")
+    df_2015_2016.to_csv(path + "df_2015_2016.csv")
+
+
 def init():
-    path = "C:\\Users\\biran\\Desktop\\3\\database.sqlite\\"
     database = path + "database.sqlite"
     """
     Country = { id , name }
@@ -127,7 +135,26 @@ def init():
         'buildUpPlayPositioningClass,defencePressureClass,defenceAggressionClass from Team_Attributes',
         conn)
 
+    data_Team = pd.read_sql_query('SELECT team_api_id, team_long_name from Team', conn)
+    data_Team = data_Team.sort_values(by=['team_api_id'])
+
+    # data_Team = data_Team.dropna(thresh=2)
+    # data_Team['team_api_id'] = data_Team['team_api_id'].astype(int)
+
     new_df = dataframe_filter(data_matchDF, data_Team_AttrDF)
+
+    # Adding team long name
+    new_df = pd.merge(data_Team, new_df, how='inner', left_on=['team_api_id'],
+                      right_on=['away_team_api_id'])
+
+    new_df = pd.merge(data_Team, new_df, how='inner', left_on=['team_api_id'],
+                      right_on=['home_team_api_id'])
+
+    del new_df['team_api_id_x']
+    del new_df['team_api_id_y']
+
+    new_df = new_df.rename(columns={'team_long_name_x': 'home_team_long_name','team_long_name_y': 'away_team_long_name'}
+                           , inplace=False)
 
     # Adding a column of binary representation win loss and draw.
     conditions = [new_df["home_team_goal"] > new_df["away_team_goal"],
@@ -137,9 +164,7 @@ def init():
     choices = ["1", "-1", "0"]
     new_df["result"] = np.select(conditions, choices, default=np.nan)
 
-    df_2012_2013_2014 = new_df.loc[(new_df['season'].isin(["2012/2013", "2013/2014", "2014/2015"]))]
-    df_2015_2016 = new_df.loc[(new_df['season'].isin(["2015/2016"]))]
-
-    # save2CVS(new_df, path)
+    test_train_models_split(new_df)
+    # save2CSV(new_df, path)
     cursor.close()
     conn.close()
