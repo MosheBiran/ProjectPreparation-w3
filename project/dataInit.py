32ed920e69a43202c2a90e32499468abdb548cb7
ShaharShc <shahar.shc@gmail.com>
2021-05-03 20:14:54 +0300

    shahar to moshe

--- project/dataInit.py
+++ project/dataInit.py
@@ -79,6 +79,7 @@ def dataframe_filter(data_match_df, data_team_attr_df):
 
     new_df_inner = new_df_inner.sort_values(by=['home_team_api_id', 'away_team_api_id', 'date'])
 
+
     return new_df_inner
 
 
@@ -100,6 +101,56 @@ def dataframe_filter_null(data_match_df, data_team_attr_df):
     new_df_outer = new_df_outer.sort_values(by=['home_team_api_id', 'away_team_api_id', 'date'])
 
     return new_df_outer
+
+def dataframe_filter_players(data_match_players_df, data_player_attr_df ):
+
+    # Clearing the date from day and month
+    data_match_players_df['date'] = data_match_players_df['date'].str.slice(stop=4)
+    data_player_attr_df['date'] = data_player_attr_df['date'].str.slice(stop=4)
+    data_player_attr_df = data_player_attr_df.groupby(['player_api_id', 'date']).mean()
+    listt = data_player_attr_df.index.values
+    id =[]
+    years = []
+    for val in listt:
+        id.append(val[0])
+        years.append(val[1])
+    data_player_attr_df['date'] = years
+    data_player_attr_df['player_api_id'] = id
+    data_player_attr_df['rating'] = data_player_attr_df['overall_rating']
+    del data_player_attr_df['overall_rating']
+    data_player_attr_df.columns.name = ''
+
+    data_match_players_df['sum'] = 0
+    data_match_players_df['count'] = 0
+
+
+
+    data_match_players_df = data_match_players_df.dropna()
+    new_df_outer = pd.merge(data_match_players_df, data_player_attr_df, how='inner', left_on=['date', 'home_player_1'],
+                            right_on=['date', 'player_api_id'])
+    data_match_players_df['sum'] = data_match_players_df['sum'] +data_match_players_df['overall_rating']
+    for i in range(1, 12):
+        new_df_outer = pd.merge(data_match_players_df, data_player_attr_df, how='inner', left_on=['date', 'home_player_'+str(i)],
+                            right_on=['date', 'team_api_id'])
+
+
+
+
+    for index_match, row in data_match_players_df.iterrows():
+        for index, rating in data_player_attr_df.iterrows():
+            if row['date'] == index[1]:
+                for i in range(1,12):
+                    if row['home_player_' + str(i)] == index[0]:
+                        row['sum'] += rating['overall_rating']
+                        row['count'] += 1
+                for i in range(1,12):
+                    if row['away_player_' + str(i)] == index[0]:
+                        row['sum'] += rating['overall_rating']
+                        row['count'] += 1
+
+    return data_match_players_df
+
+
 
 
 def init():
@@ -126,7 +177,28 @@ def init():
         'buildUpPlayPositioningClass,defencePressureClass,defenceAggressionClass from Team_Attributes',
         conn)
 
+    data_Players_AttrDF = pd.read_sql_query(
+        'SELECT player_api_id,date,overall_rating from Player_Attributes',
+        conn)
+
+    data_matchDF_players = pd.read_sql_query(
+        'SELECT home_team_api_id,away_team_api_id,season,date, home_player_1, home_player_2, home_player_3, '
+        'home_player_4, home_player_5, home_player_6, home_player_7, home_player_8, '
+        'home_player_9, home_player_10, home_player_11 , away_player_1, away_player_2, away_player_3, away_player_4, '
+        'away_player_5, away_player_6, away_player_7, away_player_8, away_player_9, away_player_10, away_player_11 '
+        'from '
+        'Match', conn)
+
+
+
+
+
+    new_player_df = dataframe_filter_players(data_matchDF_players, data_Players_AttrDF)
     new_df = dataframe_filter(data_matchDF, data_Team_AttrDF)
+
+
+
+
 
     # Adding a column of binary representation win loss and draw.
     conditions = [new_df["home_team_goal"] > new_df["away_team_goal"],
--- project/main.py
+++ project/main.py
@@ -2,5 +2,5 @@ import dataInit
 import preprocessing
 
 if __name__ == '__main__':
-    #dataInit.init()
-    preprocessing.check_data()
+    dataInit.init()
+    #preprocessing.check_data()
