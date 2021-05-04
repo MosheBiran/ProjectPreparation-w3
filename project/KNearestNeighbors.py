49c26870b25ef1966c9677e53e6bdcce358fe724
DanielDinshtein <danieldinsh@gmail.com>
2021-05-04 18:06:49 +0300

    GridSearchCV

--- project/KNearestNeighbors.py
+++ project/KNearestNeighbors.py
@@ -4,6 +4,12 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+
+
+
+# importing the metrics module - For GridCV
+from sklearn import metrics
 
 # https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
 
@@ -62,7 +68,7 @@ def model_KNN(trainData, testData):
     season_15_16_features = scaler.transform(season_15_16_features)
 
 
-    """--------------------------------- Prediction And Evaluation------------------------------------"""
+    """--------------------------------- Prediction And Evaluation Of 2015/2016 ------------------------------------"""
 
 
     prediction_on_15_16 = classifier.predict(season_15_16_features)
@@ -92,3 +98,80 @@ def model_KNN(trainData, testData):
 
     plt.show()
 
+
+def trainWithGridSearchCV(trainData, testData):
+
+    """--------------------------------- Splitting The Train Data ------------------------------------"""
+
+    X = trainData.iloc[:, :-1].values
+    y = trainData.iloc[:, len(trainData.columns) - 1].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+    """--------------------------------- Feature Scaling ------------------------------------"""
+
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
+
+    """--------------------------------- GridSearchCV Parameters ------------------------------------"""
+
+
+    leaf_size = list(range(1, 50))
+    n_neighbors = list(range(1, 35))
+    p = [1, 2]
+    meTri = ['euclidean', 'manhattan']
+
+    # making the instance
+    model = KNeighborsClassifier(n_jobs=-1)
+    # Hyper Parameters Set
+    params = {'n_neighbors': n_neighbors,
+              'leaf_size': leaf_size,
+              'weights': ['uniform', 'distance'],
+              'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+              'n_jobs': [-1],
+              'metric': meTri,
+              'p': p}
+    # Making models with hyper parameters sets
+    model1 = GridSearchCV(model, param_grid=params, cv=10, n_jobs=1)
+    # Learning
+    model1.fit(X_train, y_train)
+    # The best hyper parameters set
+    print("Best Hyper Parameters:\n", model1.best_params_)
+    # Prediction
+    prediction = model1.predict(X_test)
+
+    # evaluation(Accuracy)
+    print("Accuracy:", metrics.accuracy_score(prediction, y_test))
+    # evaluation(Confusion Matrix)
+    print("Confusion Matrix:\n", metrics.confusion_matrix(prediction, y_test))
+
+
+    """--------------------------------- *** 2015/2016 *** ------------------------------------"""
+
+    print("\n\n ************ Season 2015/2016 Prediction ************\n")
+
+    """--------------------------------- Splitting The Test Data ------------------------------------"""
+
+    season_15_16_features = testData.iloc[:, :-1].values
+    season_15_16_Result = testData.iloc[:, len(testData.columns) - 1].values
+
+    """--------------------------------- Feature Scaling ------------------------------------"""
+
+    scaler = StandardScaler()
+    scaler.fit(season_15_16_features)
+
+    season_15_16_features = scaler.transform(season_15_16_features)
+
+    """--------------------------------- Prediction And Evaluation Of 2015/2016 ------------------------------------"""
+
+    # Prediction
+    prediction = model1.predict(season_15_16_features)
+
+    # evaluation(Accuracy)
+    print("Accuracy:", metrics.accuracy_score(prediction, season_15_16_Result))
+    # evaluation(Confusion Matrix)
+    print("Confusion Matrix:\n", metrics.confusion_matrix(prediction, season_15_16_Result))
